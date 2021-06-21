c78f68c1c969f861d79e66a5c5f52455cb3de12e
Niko Strongioglou <nikolaos.strongioglou@1plusx.com>
2021-06-21 20:27:36 +0200

    Perfect squares without memoization

---
+++ src/leetcode/perfectsquares/PerfectSquares.java
@@ -0,0 +1,36 @@
+package leetcode.perfectsquares;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PerfectSquares {
+
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int numSquares(int n) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= n / 2; i++) {
+            min = Math.min(min, sum(n, 1, new Double(Math.pow(i, 2)).intValue()));
+        }
+
+        return min;
+    }
+
+    public int sum(int n,
+                   int numOfSquares,
+                   int accumulator) {
+        if (accumulator == n) {
+            return numOfSquares;
+        } else if (accumulator > n) {
+            return Integer.MAX_VALUE;
+        } else {
+            int min = Integer.MAX_VALUE;
+            for (int i = 1; i <= (n - accumulator) / 2; i++) {
+                min = Math.min(min, sum(n, numOfSquares + 1, accumulator + new Double(Math.pow(i, 2)).intValue()));
+            }
+            return min;
+        }
+    }
+
+
+}
---
+++ src/leetcode/perfectsquares/Playground.java
@@ -0,0 +1,10 @@
+package leetcode.perfectsquares;
+
+public class Playground {
+
+    public static void main(String[] args) {
+        PerfectSquares perfectSquares = new PerfectSquares();
+        int min = perfectSquares.numSquares(13);
+        System.out.println(min);
+    }
+}
