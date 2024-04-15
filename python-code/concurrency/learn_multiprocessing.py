b9e7617cf305f28aa61b3959b15876dd340f9fb5
Niko Strongioglou <nikos.strongioglou@gmail.com>
2024-04-15 11:22:44 +0200

    More concurrency shit

--- python-code/concurrency/learn_multiprocessing.py
+++ python-code/concurrency/learn_multiprocessing.py
@@ -1,12 +1,31 @@
 from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor, Future
+from typing import List, Generator
+import os
 
 
-def square(number):
+def square(number: int) -> int:
+    print(f'[process_id={os.getpid()}] producing {number * number}')
     return number * number
 
 
-if __name__ == '__main__':
+def using_process_poll_executor() -> List[Future[int]]:
+    with ProcessPoolExecutor(max_workers=3) as ppe:
+        for i in range(1, 6):
+            yield ppe.submit(square, number=i)
+        # yield [ppe.submit(square, number=i) for i in range(1, 6)]
+
+
+def using_multiprocessing_pool() -> Generator[int, None, None]:
     numbers = [1, 2, 3, 4, 5]
     with Pool(5) as pool:
-        results = pool.map(square, numbers)
-        print(results)
+        yield pool.map(square, numbers)
+
+
+if __name__ == '__main__':
+    task_futures: List[Future[int]] = using_process_poll_executor()
+    results: List[int] = [r.result() for r in task_futures]
+    print(results)
+
+    results_from_multiprocessing_pool = using_multiprocessing_pool()
+    print(list(results_from_multiprocessing_pool))
