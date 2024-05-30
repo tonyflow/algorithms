0c348ccfb728bfa425f222931978aac0f132d12a
Niko Strongioglou <nikos.strongioglou@gmail.com>
2024-05-30 16:13:16 +0200

    Version passes almost all test cases

--- python-code/minimum_number_of_days_to_make_m_bouquets.py
+++ python-code/minimum_number_of_days_to_make_m_bouquets.py
@@ -32,6 +32,9 @@ class Solution:
                     bi[1] = 1
                     bi[0] + 1 = bi[1]
 
+    A bouquet needs k adjacent flowers
+    We want to make m bouquets
+
     """
 
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
@@ -41,31 +44,51 @@ class Solution:
                 if bloomDay[i] <= days_candidate:
                     bloomed_indices.append(i)
 
+            print(f'List of bloomed indices at {days_candidate} is {bloomed_indices}')
+
             # Check how many bouquets we can make by checking how many groups
             # of adjacent flowers we have
             number_of_bouquets: int = 0
-            number_of_adjacent_bloomed: int = 1
             if k == 1:
                 number_of_bouquets = len(bloomed_indices)
             else:
-                for i in range(len(bloomed_indices) - 1):
-                    if bloomed_indices[i] + 1 == bloomed_indices[i]:
-                        number_of_adjacent_bloomed += 1
-                        if number_of_adjacent_bloomed == k:
-                            number_of_bouquets += 1
-                    else:
-                        number_of_adjacent_bloomed = 1
+                for index in bloomed_indices:
+                    can_create_bouquet = True
+                    for extender in range(1, k):
+                        print(f'Checking position {index + extender}')
+                        if index + extender not in bloomed_indices:
+                            print(
+                                f'Starting from position {index}, adjacent position {index + extender} '
+                                f'has not bloomed so we cannot proceed further')
+                            can_create_bouquet = False
+                            break
+                        else:
+                            bloomed_indices.remove(index + extender)
+                            print(f'Remaining indices {bloomed_indices}')
 
+                    if can_create_bouquet:
+                        number_of_bouquets += 1
+
+            print(f'Number of bouquets we can create with available days {days_candidate} is {number_of_bouquets}')
             return number_of_bouquets >= m
 
-        if m * k < len(bloomDay):
+        if m * k > len(bloomDay):
             return -1
 
-        left, right = 1, len(bloomDay)
+        left, right = 1, max(bloomDay)
         while left < right:
             middle: int = left + (right - left) // 2
             if can_make_m_bouquets(middle):
                 right = middle
             else:
                 left = middle + 1
-        return -1
+
+        return left
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    # print(solution.minDays(bloomDay=[1, 10, 3, 10, 2], m=3, k=1))
+    # print(solution.minDays(bloomDay=[1, 10, 3, 10, 2], m=3, k=2))
+    # print(solution.minDays(bloomDay=[7, 7, 7, 7, 12, 7, 7], m=2, k=3))
+    print(solution.minDays(bloomDay=[1, 10, 2, 9, 3, 8, 4, 7, 5, 6], m=4, k=2))
